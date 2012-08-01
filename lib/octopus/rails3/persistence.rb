1794f0f34d9491eff342fecd30aeae30fb5804f5
Patrick Sharp <psharp@numerex.com>
2012-08-01 11:56:37 -0500

    LIB-22  Use existing shard if it exists on persistance methods

--- lib/octopus/rails3/persistence.rb
+++ lib/octopus/rails3/persistence.rb
@@ -2,34 +2,59 @@ module Octopus
   module Rails3
     module Persistence
       def update_attribute(*args)
-        reload_connection()
-        super
+        possibly_wrap_connection do
+          reload_connection()
+          super
+        end
       end
 
       def update_attributes(*args)
-        reload_connection()
-        super
+        possibly_wrap_connection do
+          reload_connection()
+          super
+        end
       end
 
       def update_attributes!(*args)
-        reload_connection()
-        super
+        possibly_wrap_connection do
+          reload_connection()
+          super
+        end
       end
 
       def reload(*args)
-        reload_connection()
-        super
+        possibly_wrap_connection do
+          reload_connection()
+          super
+        end
       end
 
       def delete
-        reload_connection()
-        super
+        possibly_wrap_connection do
+          reload_connection()
+          super
+        end
       end
 
       def destroy
-        reload_connection()
-        super
+        possibly_wrap_connection do
+          reload_connection()
+          super
+        end
       end
+      
+      def should_wrap_the_connection?
+        respond_to?(:current_shard) && current_shard != nil
+      end
+
+      def possibly_wrap_connection(&block)
+        if should_wrap_the_connection?
+          Octopus.using(current_shard) { yield }
+        else
+          yield
+        end
+      end
+      
     end
   end
 end
